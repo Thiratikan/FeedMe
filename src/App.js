27a1efd29c64e3b11e17097ba63e0421d0034ae0
Thiratikan <thiratikan@gmail.com>
2022-07-18 21:30:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import item from "./data";
+import Catagories from "./Catagories";
+import Item from "./Item";
 
 function App() {
+  const [items, setItems] = useState(item);
+  const [catagories, setCategories] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="choice section">
+        <div className="title">
+          <h1>FeedMe</h1>
+        </div>
+        <Catagories />
+        <Item />
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const choice = [
+  {
+    id: 1,
+    title: "B52",
+    catagory: "drink",
+    img: "../public/images/B52.jpg",
+    desc: "coffee liqueur, Baileys,  Grand Marnier",
+  },
+  {
+    id: 2,
+    title: "Whiskey Sour",
+    catagory: "drink",
+    img: "../public/images/WhiskeySour.webp",
+    desc: "whiskey, sweet&sour, syrub, lemon, egg white, orange , cherry",
+  },
+];
