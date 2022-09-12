42a510e7960c401570a0ef7a3e036fa28087902d
TK <thiratikan@gmail.com>
2022-09-11 23:08:31 -0400

    update model box design

--- src/AddItemModelbox.js
+++ src/AddItemModelbox.js
@@ -4,6 +4,8 @@ import {
   Div,
   ButtonSection,
   Info,
+  BottomEdit,
+  TopEdit,
 } from "./styles/AddItemModelbox-styled";
 import { OverlayStyle } from "./styles/ModelBox-styled";
 
@@ -22,37 +24,47 @@ function AddItemModelbox({ isOpen, isClose }) {
         <button onClick={isClose}>X</button>
         <h1>ADD ITEM</h1>
         <Info>
-          <Div>
-            <input
-              type="text"
-              placeholder="Enter Title..."
-              id="title"
-              name="title"
-            />
-          </Div>
-          <Div>
-            <input
-              type="text"
-              placeholder="Enter Description..."
-              id="desc"
-              name="desc"
-            />
-          </Div>
-          <Div>
-            <input type="url" placeholder="Enter URL..." id="url" name="link" />
-          </Div>
-          <Div>
-            <label>Category:</label>
-            <select id="category">
-              <option>Drink</option>
-              <option>Food</option>
-              <option>Dessert</option>
-            </select>
-          </Div>
-          <Div>
-            <label>Image:</label>
-            <input type="file" id="file" accept="image" />
-          </Div>
+          <TopEdit>
+            <Div>
+              <input
+                type="text"
+                placeholder="Enter Title..."
+                id="title"
+                name="title"
+              />
+            </Div>
+            <Div>
+              <input
+                type="text"
+                placeholder="Enter Description..."
+                id="desc"
+                name="desc"
+              />
+            </Div>
+            <Div>
+              <input
+                type="url"
+                placeholder="Enter URL..."
+                id="url"
+                name="link"
+              />
+            </Div>
+          </TopEdit>
+
+          <BottomEdit>
+            <Div>
+              <label>Category: </label>
+              <select id="category">
+                <option>Drink</option>
+                <option>Food</option>
+                <option>Dessert</option>
+              </select>
+            </Div>
+            <Div>
+              <label>Image: </label>
+              <input type="file" id="file" accept="image" />
+            </Div>
+          </BottomEdit>
         </Info>
 
         <ButtonSection>
--- src/styles/AddItemModelbox-styled.js
+++ src/styles/AddItemModelbox-styled.js
@@ -7,10 +7,11 @@ export const BoxStyle = styled.div`
   background: rgba(0, 0, 0, 0.9);
   border-radius: 20px;
   width: 450px;
-  height: 500px;
+  height: 450px;
   transition: fixed;
   margin-left: auto;
   margin-right: auto;
+  margin-top: 10%;
   h1 {
     font-family: "Monoton", cursive;
     background-color: transparent;
@@ -44,6 +45,28 @@ export const BoxStyle = styled.div`
     background-color: white;
     width: 75px;
   }
+`;
+
+export const Div = styled.div`
+  background-color: transparent;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+export const ButtonSection = styled.div`
+  margin-top: 20px;
+  text-align: center;
+`;
+
+export const Info = styled.div`
+  margin-left: 10%;
+`;
+
+export const TopEdit = styled.div`
+  margin-bottom: 20px;
+
+  margin-left: 20px;
+
   input {
     color: black;
     background-color: white;
@@ -55,23 +78,14 @@ export const BoxStyle = styled.div`
     border-radius: 30px;
   }
 `;
-
-export const Div = styled.div`
-  background-color: transparent;
-  margin-top: 5px;
-  margin-bottom: 5px;
-
-  input,
-  select {
-    margin-left: 5px;
+export const BottomEdit = styled.div`
+  margin: 0px;
+  margin-left: 40px;
+  input {
+    width: 65%;
+    border-radius: 0px;
+    outline: transparent;
+    background-color: transparent;
+    text-align: center;
   }
-`;
-
-export const ButtonSection = styled.div`
-  margin-top: 20px;
-  text-align: center;
-`;
-
-export const Info = styled.div`
-  margin-left: 10%;
 `;
